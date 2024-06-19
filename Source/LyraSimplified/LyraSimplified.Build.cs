1fd6e262bd90044c6ffb0cd7b9d2f8aa2e92042f
Josie Thompson <dot.slash.josie@gmail.com>
2024-06-19 15:58:23 -0700

    initial commit - super basic content with experience and pawn data

---
+++ Source/LyraSimplified/LyraSimplified.Build.cs
@@ -0,0 +1,18 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class LyraSimplified : ModuleRules
+{
+    public LyraSimplified(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicDependencyModuleNames.AddRange(new [] {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "BasicHeroComponent",
+            "SimpleGameplayExperience"
+        });
+    }
+}
---
+++ Source/LyraSimplified/Private/LyraSimplifiedModule.cpp
@@ -0,0 +1,25 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Modules/ModuleManager.h"
+#define LOCTEXT_NAMESPACE "FLyraSimplifiedModule"
+
+class FLyraSimplifiedModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+void FLyraSimplifiedModule::StartupModule()
+{
+}
+
+void FLyraSimplifiedModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FLyraSimplifiedModule, LyraSimplified)
